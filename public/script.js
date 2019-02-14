755615e5abebfc2b455b451d53eade49954f2652
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2019-02-14 10:08:50 -0800

    updated html and css, created front-end js file

---
+++ public/script.js
@@ -0,0 +1,8 @@
+// Create variables to easily be able eo create HTML elements using keyword 'new'
+function UL(){ return document.createElement('UL')};
+function LI(){ return document.createElement('LI')};
+function H2(){ return document.createElement('H2')};
+function H3(){ return document.createElement('H3')};
+function P(){ return document.createElement('P')};
+function A(){ return document.createElement('A')};
+function IMG(){ return document.createElement('IMG')};
