5ad61c2130860d7aa1528d42b85bff5ea392f3e9
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2019-02-14 11:01:37 -0800

    updated backend to make it send html files and fix errors

--- server.js
+++ server.js
@@ -2,31 +2,39 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+// Must import body-parser module
+var bodyParser = require('body-parser');
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+// Remove '/' from before 'public' as path.join() adds slashes by default
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use('/', express.static(path.join(__dirname, 'public'));
+// Missing a closing parenthesis
+app.use('/', express.static(path.join(__dirname, 'public')));
+// Create middleware function to send favorites.html to browser on request
+app.use('/favorites', express.static(path.join(__dirname, 'public', 'favorites.html')));
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
-
-app.get('favorites', function(req, res){
+  // app.get was missing closing brace and parenthesis
+});
+// Get route was missing '/' prefix and should actually be app.post
+// as it accesses the req.body variable
+app.post('/favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
     return
-  
+    // if statement was missing closing brace
+  };
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
   fs.writeFile('./data.json', JSON.stringify(data));
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
-
-app.list(3000, function(){
+// app.list should be app.listen
+app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
